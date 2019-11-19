e2fb48b83b8c05eed59c7f8bf5a91a730a0c9eee
IlanMar <ilanmargolin@gmail.com>
2019-11-19 15:56:32 +0300

    commit 2

--- TCPserverApp/TCPserverApp/Form1.cs
+++ TCPserverApp/TCPserverApp/Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace TCPserverApp
 {
@@ -15,7 +18,9 @@ namespace TCPserverApp
         public Form1()
         {
             InitializeComponent();
+            f1 = this;            
         }
+        public static Form1 f1;
 
         private void Form1_Load(object sender, EventArgs e)
         {
